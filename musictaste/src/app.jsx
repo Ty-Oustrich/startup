4ba4d3aacbc808233432786d96aa628daf5b5c80
Tyler Oustrich <oustrichtyler@gmail.com>
2025-04-02 18:01:29 -0600

    react structure file names etc

---
+++ musictaste/src/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Analyze } from './analyze/explain-start';
+import { Leaderboard } from './leaderboard/leaderboard';
+import { About } from './about/about';
