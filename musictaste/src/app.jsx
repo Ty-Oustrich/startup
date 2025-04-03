2d9924599b3d13b0508a34197300b92b6d3886bd
Tyler Oustrich <oustrichtyler@gmail.com>
2025-04-02 18:10:49 -0600

    modified simon jsx for my project, still owkring but I have the skeleton

--- musictaste/src/app.jsx
+++ musictaste/src/app.jsx
@@ -6,3 +6,77 @@ import { Login } from './login/login';
 import { Analyze } from './analyze/explain-start';
 import { Leaderboard } from './leaderboard/leaderboard';
 import { About } from './about/about';
+
+
+export default function App() {
+    return (
+        <BrowserRouter>
+      <div className="body bg-dark text-light">
+        <header className="container-fluid">
+          <nav className="navbar fixed-top navbar-dark">
+            <div className="navbar-brand">
+              Simon<sup>&reg;</sup>
+            </div>
+            <menu className="navbar-nav">
+                <li className="nav-item">
+                <NavLink className="nav-link" to="">
+                    Login
+                </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink className="nav-link" to="analyze">
+                    Analyze
+                </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink className="nav-link" to="leaderboard">
+                    Leaderboard
+                </NavLink>
+                </li>
+                <li className="nav-item">
+                <NavLink className="nav-link" to="about">
+                    About
+                </NavLink>
+                </li>
+            </menu>
+          </nav>
+        </header>
+  
+        <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/analyze' element={<Analyze />} />
+            <Route path='/leaderboard' element={<Leaderboard />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>  
+        
+        <footer className="bg-light text-black-50">
+          <div className="container-fluid">
+            <span className="text-reset">Ty Oustrich</span>
+            <a className="text-reset" href="https://github.com/Ty-Oustrich/startup">
+              Source
+            </a>
+          </div>
+        </footer>
+      </div>
+      </BrowserRouter>
+
+      
+    );
+    function NotFound() {
+        return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+      }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
