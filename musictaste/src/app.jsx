e3a56621009c33a4233bdc1b33fb15c6c588a3e4
Tyler Oustrich <oustrichtyler@gmail.com>
2025-04-04 13:36:47 -0600

    correct name for files in routing

--- musictaste/src/app.jsx
+++ musictaste/src/app.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
-import { Analyze } from './analyze/explain-start';
+import { Analyze } from './analyze/explainStart';
 import { Leaderboard } from './leaderboard/leaderboard';
 import { About } from './about/about';
 
