665bf714fdb1767e2709d9f5000a463647925918
Tyler Oustrich <oustrichtyler@gmail.com>
2025-04-14 12:53:05 -0600

    finished simple egg page

--- musictaste/src/egg/egg.jsx
+++ musictaste/src/egg/egg.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../app.css';
 
 export function EggPage() {
     return (
-
+        <div className="bg-dark text-light min-vh-100 d-flex justify-content-center align-items-center">
+            <div className="text-center">
+                <h1 className="mb-4">Congratulations Superuser!</h1>
+                <p className="lead">You have successfully logged in.</p>
+            </div>
+        </div>
     );
 }
