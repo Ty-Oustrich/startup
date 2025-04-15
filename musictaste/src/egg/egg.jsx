5e7885cfe77b532feadfecba124e6a445c95816a
Tyler Oustrich <oustrichtyler@gmail.com>
2025-04-14 19:29:19 -0600

    superuser login in a gold box now

--- musictaste/src/egg/egg.jsx
+++ musictaste/src/egg/egg.jsx
@@ -4,9 +4,9 @@ import '../app.css';
 
 export function EggPage() {
     return (
-        <div className="bg-dark text-light min-vh-100 d-flex justify-content-center align-items-center">
-            <div className="text-center">
-                <h1 className="mb-4">Congratulations Superuser!</h1>
+        <div className="bg-dark text-light min-vh-100 d-flex justify-content-center align-items-start">
+            <div className="text-center p-4 rounded" style={{ backgroundColor: 'gold' }}>
+            <h1 className="mb-4">Congratulations Superuser!</h1>
                 <p className="lead">You have successfully logged in.</p>
             </div>
         </div>
