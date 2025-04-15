ea960acf5a93dcf11148e7c723c211518b462d6c
Tyler Oustrich <oustrichtyler@gmail.com>
2025-04-14 19:11:05 -0600

    simple max age of 7 days

--- musictaste/service/index.js
+++ musictaste/service/index.js
@@ -26,11 +26,14 @@ const superUserPassword = 'maulertwins';
 let superUser = null;
 
 function setAuthCookie(res, authToken) {
+    const maxAgeInMilliseconds = 7 * 24 * 60 * 60 * 1000; // 7 days
+
     res.cookie(authCookieName, authToken, {
         secure: true,
         httpOnly: true,
         sameSite: 'strict',
         path: '/', 
+        maxAge: maxAgeInMilliseconds,
     });
 }
 
